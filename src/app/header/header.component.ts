de081f58179c5f04e05f23e0b4e515d2d8002f9a
Felipe <felipe.silva.hts@gmail.com>
2021-03-22 18:48:50 -0300

    Aula 169 - Inserindo Rotas

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,12 @@
-import { Output } from '@angular/core';
-import { Component, OnInit, EventEmitter } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  @Output() featureSelected = new EventEmitter<string>();
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onSelect(feature:string){
-    this.featureSelected.emit(feature);
-  }
-
+  ngOnInit(): void {}
 }
